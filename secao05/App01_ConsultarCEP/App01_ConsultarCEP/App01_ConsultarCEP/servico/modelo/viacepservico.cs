13f5363bbcd78d6b0661aae19c12b9c06eacf60e
Glauco da Silva Rocha <glaucodasilvarocha@hotmail.com>
2018-10-02 18:00:59 -0300

    Executando básico.

---
+++ secao05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/servico/modelo/viacepservico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.servico.modelo;
+using  Newtonsoft.Json;
+
+namespace App01_ConsultarCEP.servico.modelo
+{
+    public class viacepservico
+    {
+        private static string _enderecoURL = "http://viacep.com.br/ws/{0}/json";
+
+        public static Endereco BuscarEnderecoViaCEP(string pCep)
+        {
+            var xNovoEnderecoURL = string.Format(_enderecoURL, pCep);
+
+            var xWc = new WebClient();
+            var xConteudo = xWc.DownloadString(xNovoEnderecoURL);
+
+            return JsonConvert.DeserializeObject<Endereco>(xConteudo);
+
+        }
+    }
+}
